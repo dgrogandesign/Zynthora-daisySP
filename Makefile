9cd05c85c0801ee926a5627b06cde5f4fa0d83d4
Danny <danny@zynthora.local>
2026-02-04 16:17:15 +0000

    Initial commit: DaisySP Synth + Web GUI

---
+++ Makefile
@@ -0,0 +1,49 @@
+CC = g++
+
+# Include all subdirectories of DaisySP/Source and DaisySP-LGPL/Source
+INCLUDES = -I. \
+	-IDaisySP/Source \
+	-IDaisySP/Source/Control \
+	-IDaisySP/Source/Drums \
+	-IDaisySP/Source/Dynamics \
+	-IDaisySP/Source/Effects \
+	-IDaisySP/Source/Filters \
+	-IDaisySP/Source/Noise \
+	-IDaisySP/Source/PhysicalModeling \
+	-IDaisySP/Source/Sampling \
+	-IDaisySP/Source/Synthesis \
+	-IDaisySP/Source/Utility \
+	-IDaisySP/DaisySP-LGPL/Source \
+	-IDaisySP/DaisySP-LGPL/Source/Effects \
+	-IDaisySP/DaisySP-LGPL/Source/Filters
+
+CFLAGS = $(INCLUDES) -O3 -Wall -D__LINUX_ALSA__
+LIBS = -lpthread -ldl -lm
+
+# DaisySP Sources (Main Library)
+DAISY_SRCS = \
+	DaisySP/Source/Control/adenv.cpp \
+	DaisySP/Source/Control/adsr.cpp \
+	DaisySP/Source/Control/phasor.cpp \
+	DaisySP/Source/Effects/flanger.cpp \
+	DaisySP/Source/Filters/svf.cpp \
+	DaisySP/Source/Synthesis/oscillator.cpp \
+	DaisySP/Source/Synthesis/variablesawosc.cpp \
+	DaisySP/Source/Utility/metro.cpp \
+	DaisySP/Source/Utility/dcblock.cpp
+
+# DaisySP LGPL Sources (Reverb, Moog Filter)
+DAISY_LGPL_SRCS = \
+	DaisySP/DaisySP-LGPL/Source/Effects/reverbsc.cpp \
+	DaisySP/DaisySP-LGPL/Source/Filters/moogladder.cpp
+
+# Main Sources
+SRCS = main.cpp mongoose.c $(DAISY_SRCS) $(DAISY_LGPL_SRCS)
+
+all: zynthora
+
+zynthora: $(SRCS)
+	$(CC) $(CFLAGS) $(SRCS) -o zynthora $(LIBS)
+
+clean:
+	rm -f zynthora
---
+++ main.cpp
@@ -0,0 +1,138 @@
+#define MINIAUDIO_IMPLEMENTATION
+#include "miniaudio.h"
+#include "mongoose.h"
+#include "DaisySP/Source/daisysp.h"
+#include "Filters/moogladder.h"
+#include "Effects/reverbsc.h"
+#include <atomic>
+#include <iostream>
+#include <string>
+
+using namespace daisysp;
+
+#define DEVICE_FORMAT       ma_format_f32
+#define DEVICE_CHANNELS     2
+#define DEVICE_SAMPLE_RATE  48000
+
+// --- GLOBAL STATE ---
+std::atomic<float> g_frequency(440.0f);
+std::atomic<float> g_amplitude(0.5f);
+std::atomic<float> g_cutoff(20000.0f);
+std::atomic<float> g_res(0.0f);
+std::atomic<int>   g_waveform(Oscillator::WAVE_SAW);
+
+// --- DSP OBJECTS ---
+Oscillator osc;
+MoogLadder flt;
+ReverbSc   verb;
+
+// --- AUDIO CALLBACK ---
+void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount)
+{
+    float* pOut = (float*)pOutput;
+    
+    // Update DSP Params from Global State
+    osc.SetFreq(g_frequency.load());
+    osc.SetAmp(g_amplitude.load());
+    osc.SetWaveform(g_waveform.load());
+    
+    flt.SetFreq(g_cutoff.load());
+    flt.SetRes(g_res.load());
+
+    for (ma_uint32 i = 0; i < frameCount; ++i) {
+        // 1. Generate
+        float sig = osc.Process();
+        
+        // 2. Filter
+        sig = flt.Process(sig);
+        
+        // 3. Reverb (Stereo)
+        float outL, outR;
+        verb.Process(sig, sig, &outL, &outR);
+
+        // 4. Output
+        pOut[i * DEVICE_CHANNELS]     = outL;
+        pOut[i * DEVICE_CHANNELS + 1] = outR;
+    }
+    (void)pInput;
+}
+
+// --- WEBSOCKET HANDLER ---
+static void fn(struct mg_connection *c, int ev, void *ev_data) {
+  if (ev == MG_EV_POLL) return; // Don't log poll
+  // std::cout << "Event: " << ev << std::endl;
+
+  if (ev == MG_EV_HTTP_MSG) {
+    struct mg_http_message *hm = (struct mg_http_message *) ev_data;
+    std::string uri(hm->uri.buf, hm->uri.len);
+    std::cout << "HTTP Request: " << uri << std::endl; // Debug Print
+
+    if (uri == "/websocket") {
+        mg_ws_upgrade(c, hm, NULL);
+    } else if (uri == "/") {
+        struct mg_http_serve_opts opts = {};
+        opts.root_dir = ".";
+        mg_http_serve_file(c, hm, "index.html", &opts);
+    } else {
+        mg_http_reply(c, 404, "", "Not Found");
+    }
+  } else if (ev == MG_EV_WS_MSG) {
+    struct mg_ws_message *wm = (struct mg_ws_message *) ev_data;
+    std::string msg(wm->data.buf, wm->data.len);
+    std::cout << "RX: " << msg << std::endl; // Debug print
+    
+    size_t colon = msg.find(':');
+    if (colon != std::string::npos) {
+        std::string cmd = msg.substr(0, colon);
+        std::string valStr = msg.substr(colon + 1);
+        try {
+            float val = std::stof(valStr);
+            if (cmd == "freq") g_frequency.store(val);
+            else if (cmd == "amp") g_amplitude.store(val);
+            else if (cmd == "cutoff") g_cutoff.store(val);
+            else if (cmd == "res") g_res.store(val);
+            else if (cmd == "wave") {
+                if (valStr == "sine") g_waveform.store(Oscillator::WAVE_SIN);
+                else if (valStr == "saw") g_waveform.store(Oscillator::WAVE_POLYBLEP_SAW);
+                else if (valStr == "square") g_waveform.store(Oscillator::WAVE_POLYBLEP_SQUARE);
+                else if (valStr == "triangle") g_waveform.store(Oscillator::WAVE_POLYBLEP_TRI);
+            }
+        } catch (...) {}
+    }
+  }
+}
+
+int main() {
+    // DSP Init
+    float sampleRate = (float)DEVICE_SAMPLE_RATE;
+    osc.Init(sampleRate);
+    flt.Init(sampleRate);
+    verb.Init(sampleRate);
+    verb.SetFeedback(0.85f);
+    verb.SetLpFreq(10000.0f);
+
+    // Audio Init
+    ma_device_config config = ma_device_config_init(ma_device_type_playback);
+    config.playback.format   = DEVICE_FORMAT;
+    config.playback.channels = DEVICE_CHANNELS;
+    config.sampleRate        = DEVICE_SAMPLE_RATE;
+    config.dataCallback      = data_callback;
+
+    ma_device device;
+    if (ma_device_init(NULL, &config, &device) != MA_SUCCESS) return -1;
+    if (ma_device_start(&device) != MA_SUCCESS) return -1;
+
+    std::cout << "Zynthora (DaisySP Edition) Started." << std::endl;
+
+    mg_log_set(0); // Disable debug logs
+
+    struct mg_mgr mgr;
+    mg_mgr_init(&mgr);
+    mg_http_listen(&mgr, "http://0.0.0.0:8000", fn, NULL);
+    
+    while (true) mg_mgr_poll(&mgr, 1000);
+
+    mg_mgr_free(&mgr);
+    ma_device_uninit(&device);
+    return 0;
+}
